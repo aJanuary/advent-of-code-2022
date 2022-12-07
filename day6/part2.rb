4d0ce587647c4b479f3d217a0f89603883bf3100
Andrew January <ajanuary@evertz.com>
2022-12-07 08:42:02 +0000

    Day 6: Add solution

---
+++ day6/part2.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+HEADER_SIZE = 14
+
+header_start = ARGF.each_char
+  .each_cons(HEADER_SIZE)
+  .each_with_index
+  .find {|chunk, index| chunk.uniq.length == HEADER_SIZE}
+  .last
+header_offset = header_start + HEADER_SIZE
+puts header_offset
