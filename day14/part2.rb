5a623717c3cb8fde65ff2cfc4397f2e35d9ed798
Andrew January <ajanuary@evertz.com>
2022-12-14 08:09:36 +0000

    Day 14: Add solution
    This solution is very slow, but I haven't had time to investigate why
    yet. My guess is it's either the creation of the new coordinate objects,
    the recursion, or a combination of both.

---
+++ day14/part2.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+Coordinate = Struct.new(:x, :y) do
+  def +(other)
+    Coordinate.new(x + other.x, y + other.y)
+  end
+end
+
+def parse_scan(stream)
+  Set.new(stream.each_line.map(&:chomp).flat_map do |line|
+    pairs = line.split(' -> ').map do |coords|
+      Coordinate.new(*coords.split(',').map(&:to_i))
+    end
+    pairs.each_cons(2).flat_map do |start_coord, end_coord|
+      if start_coord.x == end_coord.x
+        start_y = [start_coord.y, end_coord.y].min
+        end_y = [start_coord.y, end_coord.y].max
+        (start_y .. end_y).map do |y|
+          Coordinate.new(start_coord.x, y)
+        end
+      else
+        start_x = [start_coord.x, end_coord.x].min
+        end_x = [start_coord.x, end_coord.x].max
+        (start_x .. end_x).map do |x|
+          Coordinate.new(x, start_coord.y)
+        end
+      end
+    end
+  end)
+end
+
+def add_sand(map, sand, sand_coord)
+  # Hit the floor
+  return sand_coord if sand_coord.y == map.max_by(&:y).y + 1
+
+  [
+    Coordinate.new(0, 1),
+    Coordinate.new(-1, 1),
+    Coordinate.new(1, 1)
+  ].each do |drop_delta|
+    potential_sand_coord = sand_coord + drop_delta
+    if !map.include?(potential_sand_coord) && !sand.include?(potential_sand_coord)
+      return add_sand(map, sand, potential_sand_coord)
+    end
+  end
+  
+  sand_coord
+end
+
+SOURCE = Coordinate.new(500, 0)
+
+map = parse_scan(ARGF)
+
+sand = Set.new
+loop do
+  sand_coord = add_sand(map, sand, SOURCE)
+  sand << sand_coord
+  break if sand_coord == SOURCE
+end
+
+puts sand.size
