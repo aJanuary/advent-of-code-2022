e3d39db0f227eae339c41a15f8774fecbc6fcffc
Andrew January <ajanuary@evertz.com>
2022-12-15 10:53:38 +0000

    Day 15: Speed up part 2 by using a more sophisticated algorithm.
    This speeds it up from 1m15s to 57ms.

--- day15/part2.rb
+++ day15/part2.rb
@@ -16,7 +16,7 @@ def taxicab_distance(a, b)
   (a.x - b.x).abs + (a.y - b.y).abs
 end
 
- sensors = ARGF.each_line.map do |line|
+sensors = ARGF.each_line.map do |line|
   sensor_x, sensor_y, beacon_x, beacon_y = line.scan(/-?\d+/).map(&:to_i)
   sensor_coord = Coordinate.new(sensor_x, sensor_y)
   beacon_coord = Coordinate.new(beacon_x, beacon_y)
@@ -24,39 +24,51 @@ end
   Sensor.new(sensor_coord, scan_distance)
 end
 
-def is_distress_signal(sensors, candidate_coord)
-  return if !COORD_RANGE.include?(candidate_coord.x) || !COORD_RANGE.include?(candidate_coord.y)
+# Because we know there is only one spot for the beacon, we know it must be
+# surrounded on all sides by the edge of a sensor's range. If one side wasn't,
+# then that would leave another open spot that the beacon could be in.
+# Characterize the edges of the sensor range as lines defined as mx + c.
+# Positive edges have m=1, negative edges have m=-1. c is the intersection with
+# the y axis.
+# The beacon must be at the intersection of the lines where two or more positive
+# edges overlap, and two or more negative edges overlap.
+# So start by finding the lines, then filter out to just the ones that overlap.
+# Then look at the combination of all possible positive and negative edges, and
+# find their intersection points. This forms our pool of candidates.
+# We can then look at each candidate and find the one that is not reachable by
+# any sensor.
+positive_edges = sensors.flat_map do |sensor|
+  [
+    sensor.coord.y - (sensor.coord.x - sensor.scan_distance - 1),
+    sensor.coord.y - (sensor.coord.x + sensor.scan_distance + 1),
+  ]
+end
 
+negative_edges = sensors.flat_map do |sensor|
+  [
+    sensor.coord.y + (sensor.coord.x + sensor.scan_distance + 1),
+    sensor.coord.y + (sensor.coord.x - sensor.scan_distance - 1),
+  ]
+end
+
+overlapping_positive_edges = positive_edges.group_by {|x| x}.select {|k, v| v.length > 1}.map {|k, v| k}
+overlapping_negative_edges = negative_edges.group_by {|x| x}.select {|k, v| v.length > 1}.map {|k, v| k}
+
+candidates = overlapping_positive_edges.product(overlapping_negative_edges).flat_map do |positive_edge, negative_edge|
+  x_intersection = (negative_edge - positive_edge) / 2
+  y_intersection = x_intersection + positive_edge
+  if COORD_RANGE.include?(x_intersection) && COORD_RANGE.include?(y_intersection)
+    [Coordinate.new(x_intersection, y_intersection)]
+  else
+    []
+  end
+end
+
+beacon_coord = candidates.find do |coord|
   sensors.all? do |sensor|
-    taxicab_distance(candidate_coord, sensor.coord) > sensor.scan_distance
+    taxicab_distance(coord, sensor.coord) > sensor.scan_distance
   end
 end
 
-def find_distress_signal_coords(sensors)
-  # Because there is only one possible location, the distress signal can only be
-  # just outside the edge of one or more sensors. So rather than check the
-  # trillion possible locations, just check the edges of the sensor ranges.
-  sensors.each_with_index do |sensor, i|
-    coord = Coordinate.new(sensor.coord.x + sensor.scan_distance + 1, sensor.coord.y)
-
-    until coord.x == sensor.coord.x
-      coord += Coordinate.new(-1, 1)
-      return coord if is_distress_signal(sensors, coord)
-    end
-    until coord.y == sensor.coord.y
-      coord += Coordinate.new(-1, -1)
-      return coord if is_distress_signal(sensors, coord)
-    end
-    until coord.x == sensor.coord.x
-      coord += Coordinate.new(1, -1)
-      return coord if is_distress_signal(sensors, coord)
-    end
-    until coord.x == sensor.coord.x + sensor.scan_distance + 1
-      coord += Coordinate.new(1, 1)
-      return coord if is_distress_signal(sensors, coord)
-    end
-  end
-end
-
-distress_signal_coords = find_distress_signal_coords(sensors)
-puts (distress_signal_coords.x * 4000000) + distress_signal_coords.y
+tuning_freq = beacon_coord.x * 4000000 + beacon_coord.y
+puts tuning_freq
